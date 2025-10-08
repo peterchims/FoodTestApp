fa08e400b286440b4229394488ac7d09a208f3dd
Ogba Peter Chima <peter@skilledup.life>
2025-10-08 04:40:05 +0100

    Database-configuration-setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import { ENV } from "./config/env.js";
 
-dotenv.config();
+
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = ENV.PORT || 5001;
 
 // Middlewares
 app.use(cors());
@@ -14,6 +14,10 @@ app.use(express.json());
 // Basic route
 app.get("/", (req, res) => {
   res.json({ message: "Server is running successfully 🚀" });
+});
+
+app.get("/api/health", (req,res) => {
+  res.status(200).json({success:true});
 });
 
 // Start server
