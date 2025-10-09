ad06f405ce1165dc2a1f59710fc917e0924f95b1
Ogba Peter Chima <peter@skilledup.life>
2025-10-09 14:55:23 +0100

    React-App-Dev

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View , StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Edit app/index.tsx to edit this screen .</Text>
+    <Text style={styles.text}> This is another text</Text>
+    
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+  },
+  text: {
+    color:"red",
+    fontSize:30,
+    fontWeight:"bold",
+    fontFamily:"inter"
+  },
+  
+})
